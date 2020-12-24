fc3ff20b5a41c0a82259715a79ddccbdbb3466f3
Gabriel <gaplogan@gmail.com>
2020-12-24 15:41:58 -0300

    2020.12.24.15.41

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cs_1._8_AvisoFesta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoje vai ter festa na casa da Fabi");
+            Console.WriteLine("Hoje vai ter festa na casa da Fabi");
+            Console.WriteLine("Hoje vai ter festa na casa da Fabi");
+            Console.WriteLine("Hoje vai ter festa na casa da Fabi");
+            Console.WriteLine("Hoje vai ter festa na casa da Fabi");
+        }
+    }
+}
